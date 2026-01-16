0f4f88ba198379265d1884c72f0bb42dce0d8132
tantaneity <157833402+tantaneity@users.noreply.github.com>
2026-01-16 20:44:43 +0200

    fix: add default values to settings for development

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    database_url: str
-    cat_api_url: str
+    database_url: str = "postgresql://spycat:spycat@localhost:5432/spycatdb"
+    cat_api_url: str = "https://api.thecatapi.com/v1/breeds"
 
     class Config:
         env_file = ".env"
